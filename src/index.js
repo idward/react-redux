f5d26c7bfd156170d58a475b35bd99cb87c7805d
idwardluo <apple@idwardluo.local>
2017-12-04 11:37:37 +0800

    update react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import logger from 'redux-logger';
+
+import store from './store/store';
 
 import App from './App';
 
@@ -12,50 +11,10 @@ import App from './App';
 //     lastValues: []
 // }
 
-const mathReducer = (state = {result: 1, lastValues: []}, action) => {
-    switch (action.type) {
-        case 'ADD':
-            // state.result += action.payload;
-            return {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-        case 'SUBTRACT':
-            return state = state - action.payload;
-        default:
-            return state;
-    }
-}
-
-const userReducer = (state = {name: 'Will', age: 27}, action) => {
-    switch (action.type) {
-        case 'SET_NAME':
-            // state.result += action.payload;
-            return {
-                ...state,
-                name: action.payload
-            };
-        case 'SET_AGE':
-            return {
-                ...state,
-                age: action.payload
-            };
-        default:
-            return state;
-    }
-}
-
 // const myLogger = (store) => (next) => (action) => {
 //     console.log('Logged action: ', action);
 //     next(action);
 // };
-
-const store = createStore(combineReducers(
-    {mathReducer, userReducer}),
-    {},
-    applyMiddleware(logger)
-);
 
 // store.subscribe(() => {
 //     console.log('Store updated: ', store.getState());
